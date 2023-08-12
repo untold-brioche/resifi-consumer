66b58c2a07a5dc00bb5d4dea45cbc8c6631cf087
dogeystamp <dogeystamp@disroot.org>
2023-08-12 15:51:49 -0400

    src/bin/ReturnView: added skeleton form

--- src/bin/ReturnView.ts
+++ src/bin/ReturnView.ts
@@ -4,7 +4,14 @@ const ReturnView: Component = {
 	view: () => {
 		return [
 			m("h1", "Return an item"),
-			m("p", "We're sorry that our item could not satisfy your needs. Please fill out this form to initiate a refund.")
+			m("p", "We're sorry that our item could not satisfy your needs. Please fill out this form to initiate a refund."),
+			m("br"),
+			m("label", "Your address"),
+			m("input.form-textbox[type=text]"),
+			m("br"),
+			m("button", "Initiate refund"),
+			m("b", "Cup, 1pc."),
+			m("img", {src: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Mug_of_Tea.JPG/800px-Mug_of_Tea.JPG"}),
 		]
 	}
 }
